fe6a90aef40be4b8b153ef5eea47237f3c185ec1
malpa-iscteiulpt <malpa@iscte-iul.pt>
2019-04-09 15:57:14 +0100

    dik

--- app/src/main/java/com/example/ltfx/crumbs/MainActivity.java
+++ app/src/main/java/com/example/ltfx/crumbs/MainActivity.java
@@ -91,8 +91,6 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
-            return true;
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
