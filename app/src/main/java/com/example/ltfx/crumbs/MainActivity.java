f23682d56c4514f85950207631284b041f38190f
dacqo-iscteiulpt <dacqo@iscte-iul.pt>
2019-04-09 15:54:12 +0100

    Login Base Activity

--- app/src/main/java/com/example/ltfx/crumbs/MainActivity.java
+++ app/src/main/java/com/example/ltfx/crumbs/MainActivity.java
@@ -91,7 +91,6 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
-            return true;
 
         }
 
